dbef808e65300b84250b9d549d7e0762aaa27829
Daniel Krzeminski <dankrzeminski32@gmail.com>
2021-02-16 18:57:36 -0600

    ready for deployement

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -4,4 +4,5 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    link = models.CharField(max_length=140, default = 'DEFAULT VALUE')
     image = models.FilePathField(path="/img")
